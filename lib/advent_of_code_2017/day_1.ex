47891b645905cb492a308f3ca2bae8b73551db71
Ed Ellson <eellson@gmail.com>
2017-12-01 14:01:22 +0000

    Solve day 1 part 2
    I reconsidered how to solve part 1 after having done this, so I could
    use the same strategy for both. Rather than relying on pattern matching
    with a custom reduce-type function, I now create the pairs I want to
    compare in advance, then reduce against these based on whether the
    pairings are equal or not.
    
    I don't love it, but it works, and I have some generalisation by taking
    this path.

--- lib/advent_of_code_2017/day_1.ex
+++ lib/advent_of_code_2017/day_1.ex
@@ -1,6 +1,56 @@
 defmodule AdventOfCode2017.Day1 do
-  def run() do
-    "./lib/advent_of_code_2017/day_1/input.txt"
+  @input_path "./lib/advent_of_code_2017/day_1/input.txt"
+
+  def solve_part_1() do
+    @input_path
+    |> parse()
+    |> part_1()
+  end
+
+  def solve_part_2() do
+    @input_path
+    |> parse()
+    |> part_2()
+  end
+
+  def part_1(digits) do
+    digits
+    |> pair_subsequent()
+    |> sum_matching()
+  end
+
+  def part_2(digits) do
+    digits
+    |> pair_opposite()
+    |> sum_matching()
+  end
+
+  def pair_subsequent(digits) do
+    [first|rest] = digits
+    rotate = rest ++ [first]
+
+    digits
+    |> Enum.zip(rotate)
+  end
+
+  def pair_opposite(digits) do
+    len = Enum.count(digits)
+    {first, second} = Enum.split(digits, Integer.floor_div(len, 2))
+    rotate = second ++ first
+
+    digits
+    |> Enum.zip(rotate)
+  end
+
+  defp sum_matching(pairs) do
+    Enum.reduce(pairs, 0, fn
+      {n,n}, total -> total + n
+      _, total -> total
+    end)
+  end
+
+  defp parse(path) do
+    path
     |> File.stream!()
     |> Enum.reject(&comment_or_empty?(&1))
     |> List.flatten()
@@ -8,22 +58,6 @@ defmodule AdventOfCode2017.Day1 do
     |> String.trim_trailing()
     |> String.graphemes()
     |> Enum.map(&String.to_integer/1)
-    |> sum()
-  end
-
-  def sum(digits), do: do_sum(0, [], digits)
-
-  defp do_sum(accum, done, [n|[n|_]=next]) do
-    do_sum(accum + n, [n|done], next)
-  end
-  defp do_sum(accum, done, [n]) do
-    case Enum.reverse(done) do
-      [^n|_] -> accum + n
-      _ -> accum
-    end
-  end
-  defp do_sum(accum, done, [n|next]) do
-    do_sum(accum, [n|done], next)
   end
 
   defp comment_or_empty?(string) do
--- test/advent_of_code_2017/day_1_test.exs
+++ test/advent_of_code_2017/day_1_test.exs
@@ -2,10 +2,18 @@ defmodule AdventOfCode2017.Day1Test do
   use ExUnit.Case
   alias AdventOfCode2017.Day1
 
-  test "sum/1 sums digits matching subsequent digit" do
-    assert Day1.sum([1,1,2,2]) == 3
-    assert Day1.sum([1,1,1,1]) == 4
-    assert Day1.sum([1,2,3,4]) == 0
-    assert Day1.sum([9,1,2,1,2,1,2,9]) == 9
+  test "part_1/1 sums digits matching subsequent digit" do
+    assert Day1.part_1([1,1,2,2]) == 3
+    assert Day1.part_1([1,1,1,1]) == 4
+    assert Day1.part_1([1,2,3,4]) == 0
+    assert Day1.part_1([9,1,2,1,2,1,2,9]) == 9
+  end
+
+  test "part_2/1 sums matching opposite digit" do
+    assert Day1.part_2([1,2,1,2]) == 6
+    assert Day1.part_2([1,2,2,1]) == 0
+    assert Day1.part_2([1,2,3,4,2,5]) == 4
+    assert Day1.part_2([1,2,3,1,2,3]) == 12
+    assert Day1.part_2([1,2,1,3,1,4,1,5]) == 4
   end
 end
