d64196098c1a49de4827e45bd4be6c66817e1558
Ed Ellson <eellson@gmail.com>
2017-12-01 12:46:23 +0000

    Solve day 1
    This gives the correct answer!
    
    I think the input parsing logic could be improved, and likely moved out
    into some helpers module. Gonna wait and see if it ends up useful
    elsewhere first though.

--- lib/advent_of_code_2017/day_1.ex
+++ lib/advent_of_code_2017/day_1.ex
@@ -1,19 +1,32 @@
 defmodule AdventOfCode2017.Day1 do
+  def run() do
+    "./lib/advent_of_code_2017/day_1/input.txt"
+    |> File.stream!()
+    |> Enum.reject(&comment_or_empty?(&1))
+    |> List.flatten()
+    |> List.first()
+    |> String.trim_trailing()
+    |> String.graphemes()
+    |> Enum.map(&String.to_integer/1)
+    |> sum()
+  end
+
   def sum(digits), do: do_sum(0, [], digits)
 
-  defp do_sum(accum, done, [n|[n|rest]=next]) do
-    # IO.inspect {done, n, next}
+  defp do_sum(accum, done, [n|[n|_]=next]) do
     do_sum(accum + n, [n|done], next)
   end
   defp do_sum(accum, done, [n]) do
-    # IO.inspect {done, n}
     case Enum.reverse(done) do
       [^n|_] -> accum + n
       _ -> accum
     end
   end
   defp do_sum(accum, done, [n|next]) do
-    # IO.inspect {done, n, n, next}
     do_sum(accum, [n|done], next)
+  end
+
+  defp comment_or_empty?(string) do
+    String.starts_with?(string, "//") || string == "\n"
   end
 end
