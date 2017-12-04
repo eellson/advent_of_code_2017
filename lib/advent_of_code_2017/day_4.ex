96087ea31b003658b1288d2f5057606504c8b739
Ed Ellson <eellson@gmail.com>
2017-12-04 13:04:58 +0000

    Solve Day1 part1

--- lib/advent_of_code_2017/day_4.ex
+++ lib/advent_of_code_2017/day_4.ex
@@ -1,12 +1,24 @@
 defmodule AdventOfCode2017.Day4 do
-  # A new system policy has been put in place that requires all accounts to use a passphrase instead of simply a password. A passphrase consists of a series of words (lowercase letters) separated by spaces.
+  @input_path "./lib/advent_of_code_2017/day_4/input.txt"
 
-  # To ensure security, a valid passphrase must contain no duplicate words.
+  def solve_part_1 do
+    @input_path
+    |> parse()
+    |> part_1()
+  end
 
-  # For example:
+  def part_1(input) do
+    input
+    |> Enum.count(&(Enum.uniq(&1) == &1))
+  end
 
-  # aa bb cc dd ee is valid.
-  # aa bb cc dd aa is not valid - the word aa appears more than once.
-  # aa bb cc dd aaa is valid - aa and aaa count as different words.
-  # The system's full passphrase list is available as your puzzle input. How many passphrases are valid?
+  def parse(path) do
+    path
+    |> File.stream!()
+    |> Enum.map(fn line ->
+      line
+      |> String.trim()
+      |> String.split()
+    end)
+  end
 end
